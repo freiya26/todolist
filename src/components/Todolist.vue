9af63527adcc428c16b11ee5f1e15ae42b8c8e38
freiya26 <redlady92@naver.com>
2022-04-02 08:37:44 +0000

    투두리스트 뷰 적용

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -3,7 +3,6 @@
     <main class="todo">
         <div class="container">
             <h1>To - Do List</h1>
-
             <!-- ul.list_add -->
             <ul class="list_add">
                 <li class="list_add_tit"><p>등록 하기</p></li>
@@ -14,46 +13,13 @@
             <!-- ul.todo_list -->
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li>
-
-
-
-                <li class="del_btn">
-                    <p>
-                        <input type="checkbox" name="check1" value="">JS 공부하기
-                    </p>
-                    <ul class="todo_list_btn">
-                        <li><button>수 정</button></li>
-                        <li><button>삭 제</button></li>
-                    </ul>
-                </li>
-
-
-
-                <li>
-                    <p><input type="checkbox" name="check2" value="">웹 알아보기</p>
-                    <p><span></span></p>
-                </li>
-                <li>
-                    <p><input type="checkbox" name="check3" value="">책 읽기</p>
-                    <p><span></span></p>
-                </li>
-                <li>
-                    <p><input type="checkbox" name="check4" value="">거북이한테 전화하기</p>
-                    <p><span></span></p>
-                </li>
-                <li>
-                    <p><input type="checkbox" name="check5" value="">유리랑 소꿉놀이하기</p>
-                    <p><span></span></p>
+                <li class="del_btn" v-for="(item,index) in todolist" :key="index">
+                    <p><input type="checkbox" name="check1" value="">{{ item.todo }}</p>
                 </li>
             </ul>
             <!--// ul.todo_list -->
-
         </div>
-    </main>
-	  
-	  
-	  
-	  
+    </main>	  
   </div>
 </template>
 
@@ -62,6 +28,13 @@ export default {
   name: 'todolist',
   data: function() {  
 	  return {
+		  todolist:[
+			  {todo:"Vue.js 공부하기"},
+			  {todo:"독서 2p 이상"},
+			  {todo:"과제하기"},
+			  {todo:"9시-스터디하기"},
+			  {todo:"과제 채점"},
+		  ]
 	  }
   },
 }
