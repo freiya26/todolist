82eccb4cac57988d46ce25a06f9a65ae26c153a3
freiya26 <redlady92@naver.com>
2022-04-09 08:45:02 +0000

    완료 투두 삭제

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -7,7 +7,9 @@
 
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li>
-				<li><p> {{ remaining }} / {{ todolist.length }} 건 처리 </p></li>
+				<li><p> {{ remaining }} / {{ todolist.length }} 건 처리 </p>
+					<button @click="cleanTodo"> 처리완료삭제 </button>
+				</li>
                 <li class="del_btn" v-for="(item,index) in todolist" :key="index">
                     <p :class="{doneStyle:item.done}"><input type="checkbox" name="check1" v-model="item.done">{{ item.todo }}</p>
 					<ul class="todo_list_btn">
@@ -53,6 +55,11 @@ export default {
 	  },
 	  subTodo(idx){
 		this.todolist.splice(idx, 1);
+	  },
+	  cleanTodo(){
+		this.todolist = this.todolist.filter(function(val){
+			return val.done == false;
+		});
 	  }
   }
 }
