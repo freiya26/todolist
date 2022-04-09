80bee530a3a08139020b48b2dfba91165282b7d2
freiya26 <redlady92@naver.com>
2022-04-09 08:15:41 +0000

    투두리스트 추가 삭제 완료 기능구현

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -3,39 +3,56 @@
     <main class="todo">
         <div class="container">
             <h1>To - Do List</h1>
-            <!-- ul.list_add -->
-            <ul class="list_add">
-                <li class="list_add_tit"><p>등록 하기</p></li>
-                <li><p><input type="text"><button>등 록</button></p></li>
-            </ul>
-            <!--// ul.list_add -->
+				<TodoInput @addTodo="addTodo"/>
 
-            <!-- ul.todo_list -->
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li>
                 <li class="del_btn" v-for="(item,index) in todolist" :key="index">
-                    <p><input type="checkbox" name="check1" value="">{{ item.todo }}</p>
+                    <p :class="{doneStyle:item.done}"><input type="checkbox" name="check1" v-model="item.done">{{ item.todo }}</p>
+					<ul class="todo_list_btn">
+                        <li><button @click="subTodo(index)">삭 제</button></li>
+                    </ul>
                 </li>
             </ul>
-            <!--// ul.todo_list -->
+
         </div>
     </main>	  
   </div>
 </template>
 
 <script>
+import TodoInput from './TodoInput.vue'
+	
 export default {
   name: 'todolist',
+  components: {
+	  TodoInput
+  },
   data: function() {  
 	  return {
 		  todolist:[
-			  {todo:"Vue.js 공부하기"},
-			  {todo:"독서 2p 이상"},
-			  {todo:"과제하기"},
-			  {todo:"9시-스터디하기"},
-			  {todo:"과제 채점"},
+			  {done:false, todo:"Vue.js 공부하기"},
+			  {done:false, todo:"독서 2p 이상"},
+			  {done:false, todo:"과제하기"},
+			  {done:false, todo:"9시-스터디하기"},
+			  {done:false, todo:"과제 채점"},
 		  ]
 	  }
   },
+  methods:{
+	  addTodo(val){
+		this.todolist.push({done:false, todo:val});
+	  },
+	  subTodo(idx){
+		this.todolist.splice(idx, 1);
+	  }
+  }
 }
 </script>
+
+<style>
+	.doneStyle {
+		text-decoration : line-through;
+		color: lightgray;
+	}	
+</style>
